4a51d75dcee63c9f71ee39d2a6a9027390fb8f5f
xiang.yuan <yuan_xiang2020@163.com>
2024-10-08 17:31:27 +0800

    add gin webapp

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	config "mqtt_pro/config"
 	"mqtt_pro/mq_client"
 	"mqtt_pro/utils"
-	"time"
+	"net/http"
 )
 
 func SubAllMqttMessage(MqClientHandler []mq_client.MqClientHandler) error {
@@ -17,6 +18,31 @@ func SubAllMqttMessage(MqClientHandler []mq_client.MqClientHandler) error {
 	return nil
 }
 
+func MqProcess(cfg *config.Config) {
+	// Sub All Messages
+	err := SubAllMqttMessage(mq_client.InitMqClient(cfg))
+	if err != nil {
+		panic(err)
+	}
+	// Loop to maintain client connectivity
+	//for {
+	//	time.Sleep(10 * time.Millisecond)
+	//}
+}
+
+func WebApp() error {
+	r := gin.Default()
+	// 返回一个json数据
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"code":    http.StatusOK,
+			"message": "ok",
+		})
+	})
+
+	return r.Run()
+}
+
 func main() {
 	cfg, err := config.InitConfig()
 	if err != nil {
@@ -26,13 +52,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// Sub All Messages
-	err = SubAllMqttMessage(mq_client.InitMqClient(cfg))
+	go MqProcess(cfg)
+	err = WebApp()
 	if err != nil {
 		panic(err)
-	}
-	// Loop to maintain client connectivity
-	for {
-		time.Sleep(10 * time.Millisecond)
 	}
 }
