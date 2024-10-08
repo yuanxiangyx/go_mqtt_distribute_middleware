6741daf9773aa42a4224c53ddbf01014a69e6a68
xiang.yuan <yuan_xiang2020@163.com>
2024-10-08 14:54:45 +0800

    update Dockerfile

--- build_image.sh
+++ build_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 now=`date -d today +"%Y-%m-%d-%H-%M-%S"`
-img=mqtt_brige:$now
+img=mqtt_bridge:$now
 
 docker build -t $img .
 
 rs="last build ok: \n$img\n"
 
 echo -e "$rs"
-echo -e "$rs" >> last_build_mqtt_brige.txt
+echo -e "$rs" >> last_build_mqtt_bridge.txt
